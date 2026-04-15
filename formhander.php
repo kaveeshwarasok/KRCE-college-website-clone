15b0e76c342a8a824110aec8749732221394b856
kaveeshwarasok <kaveeshwarasok@gmail.com>
2026-04-15 12:57:47 +0530

    Update formhander.php

--- formhander.php
+++ formhander.php
@@ -5,14 +5,14 @@ $subject =$_POST['subject'];
 $message =$_POST['message'];
 
 
-$email_from='naveenrameshemr@gmail.com';
+$email_from='kaveeshwarasok@gmail.com';
 $email_subject='New Form submission';
 $email_body="User Name:$name.\n ".
             "User Email:$visitor_email.\n ".
             "User Subject:$subject.\n ".
             "User Message:$message.\n ";
 
-$to ='naveenrameshemr@gmail.com';
+$to ='kaveeshwarasok@gmail.com';
 $headers="From: $email_from \r\n";
 $headers .="Replay to:$visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
